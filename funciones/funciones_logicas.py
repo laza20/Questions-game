96da3c55f9d8756a4dc5eece1431390a3dffe077
lazaro20 <salvirialazaro@gmail.com>
2025-08-10 17:04:55 -0300

    Feat(funciones/funciones_logicas.py - funciones/funciones_carga.py): Crear funciones de carga, con la cual permitimos la carga de muchos documentos en la base de datos y otra funcion la cual permite la carga de un solo documento. Crear funciones logicas para facilitar la construccion del programa, entre ellas una para validar un oid o retornar un error, otra para validar el oid o retornar false y una para buscar data.

---
+++ funciones/funciones_logicas.py
@@ -0,0 +1,27 @@
+from bson import ObjectId
+from bson.errors import InvalidId
+from fastapi import HTTPException, status
+from db.client import db_client
+from typing import get_type_hints
+
+
+def validate_object_id(id: str):
+    try:
+        return ObjectId(id)
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="ID inválido")
+    
+def validate_object_id_or_false(id: str):
+    try:
+        return ObjectId(id)
+    except InvalidId:
+        return False
+    
+def buscar_data(id, base_de_datos_2):
+    coleccion = getattr(db_client, base_de_datos_2)
+    oid = validate_object_id(id)
+    data = coleccion.find_one({"_id":oid})
+    if not data:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Dato incorrecta")
+    
+    return data
