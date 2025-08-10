80a9e68c21506c389e0d8303d50fc5c21d8b0a8a
lazaro20 <salvirialazaro@gmail.com>
2025-08-10 19:12:45 -0300

    Feat(routers/): Crear un router para categoria y usar la funcion para realizar la carga de un documento.

---
+++ routers/categorias.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from db.models.categorias import Categoria
+from db.schemas.categorias import categoria_schema,categorias_schema
+from peticiones_http import (
+    peticiones_http_post
+)
+from validaciones import validar_categorias
+
+
+
+router = APIRouter(prefix="/Categoria",
+                   tags=["CATEGORIA"],
+                   responses={404:{"Message":"No encontrado"}}
+)
+base_de_datos = "Categoria"
+
+peticiones_http_post.cargar_uno(
+    Categoria,
+    router,
+    base_de_datos,
+    categoria_schema,
+    validar_categorias.validacion_carga_categoria
+)
