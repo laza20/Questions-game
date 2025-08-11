62f27c29406d2ea285c1c4cef177050d7c819978
lazaro20 <salvirialazaro@gmail.com>
2025-08-11 10:33:12 -0300

    Feat(validaciones_generales/lista_campos.py): Agregar las propiedades de subcategoria a la lista de campos.

--- validaciones_generales/lista_campos.py
+++ validaciones_generales/lista_campos.py
@@ -1,3 +1,4 @@
 listas_de_campos = {
-    "Categoria": ["_id", "nombre_categoria", "descripcion", "tipo", "estado"]
+    "Categoria": ["_id", "nombre_categoria", "descripcion", "tipo", "estado"],
+    "Sub_categoria": ["_id", "nombre","categoria_principal", "descripcion", "tipo", "estado"]
 }
