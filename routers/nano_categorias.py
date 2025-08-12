ba02366174c4bbb569cbf75d6c6c4f70b7393ef7
lazaro20 <salvirialazaro@gmail.com>
2025-08-12 09:26:07 -0300

    Feat(routers/nano_categorias.py): Crear el router para el correcto funcionamiento de nano_categorias. Funcional para cargar un documento y muchos documentos.

---
+++ routers/nano_categorias.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter
+from db.models.nano_categoria import NanoCategoria
+from db.schemas.nano_categorias import nano_categoria_schema,nano_categorias_schema
+from peticiones_http import (
+    peticiones_http_post
+)
+from validaciones import validar_nano_categoria
+
+
+
+router = APIRouter(prefix="/Nano/Categoria",
+                   tags=["NANO CATEGORIA"],
+                   responses={404:{"Message":"No encontrado"}}
+)
+base_de_datos = "Nano_categoria"
+
+peticiones_http_post.cargar_uno(
+    NanoCategoria,
+    router,
+    base_de_datos,
+    nano_categoria_schema,
+    validar_nano_categoria.validacion_carga_nano_categoria
+)
+
+peticiones_http_post.cargar_muchos(
+    NanoCategoria,
+    router,
+    base_de_datos,
+    nano_categorias_schema,
+    validar_nano_categoria.validacion_carga_nano_categoria
+)
