2a9df92eac9360b70bc7616b3d406077da91d2c6
lazaro20 <salvirialazaro@gmail.com>
2025-08-12 09:11:27 -0300

    Feat(validaciones_generales/lista_campos.py): Agregar los nuevos campos a la lista.

--- validaciones_generales/lista_campos.py
+++ validaciones_generales/lista_campos.py
@@ -1,4 +1,6 @@
 listas_de_campos = {
     "Categoria": ["_id", "nombre_categoria", "descripcion", "tipo", "estado"],
-    "Sub_categoria": ["_id", "nombre","categoria_principal", "descripcion", "tipo", "estado"]
+    "Sub_categoria": ["_id", "nombre","categoria_principal", "descripcion", "tipo", "estado"],
+    "Micro_categoria": ["_id", "nombre","sub_categoria", "descripcion", "tipo", "estado"],
+    "Nano_categoria": ["_id", "nombre","micro_categoria", "descripcion", "tipo", "estado"]
 }
