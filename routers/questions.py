b25f38e3e9cc2f4d4f0a2f139ef01a9a1212f157
lazaro20 <salvirialazaro@gmail.com>
2025-08-14 17:22:07 -0300

    Feat(routers/questions.py): Crear el router para realizar poder manipular los documentos de preguntas.

---
+++ routers/questions.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from db.models.questions import Question
+from db.schemas.nano_categorias import nano_categoria_schema,nano_categorias_schema
+from peticiones_http import (
+    peticiones_http_post,
+    peticiones_http_get
+)
+from validaciones import validar_questions
+
+
+
+router = APIRouter(prefix="/Preguntas",
+                   tags=["PREGUNTAS"],
+                   responses={404:{"Message":"No encontrado"}}
+)
+base_de_datos = "Preguntas"
+
+peticiones_http_post.realizar_carga(
+    Question,
+    router,
+    base_de_datos,
+    validar_questions.validacion_carga_question
+)
