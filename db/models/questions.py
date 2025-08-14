91539640227705eda8fa47d674665e9fca643daf
lazaro20 <salvirialazaro@gmail.com>
2025-08-14 17:20:00 -0300

    Feat(db/models/questions.py): Crear un archivo que contenga los modelos necesarios para realizar las preguntas.

---
+++ db/models/questions.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import List, Optional, Dict
+from pydantic import BaseModel, Field
+
+class Category(BaseModel):
+    categoria:  Optional[str] = None
+    sub_categoria: Optional[str] = None
+    micro_categoria: Optional[str] = None
+    nano_categoria: Optional[str] = None
+
+class Question(BaseModel):
+    id: str | None = None
+    pregunta: str
+    opciones: List[str]
+    respuesta_correcta: str
+    categoria: Category  # Aquí la jerarquía es un modelo anidado
+    usuario_carga: str
+    fecha_carga: Optional[datetime] = None
+    tipo: Optional[datetime] = None
+    estado: bool
+
+class QuestionRequest(BaseModel):
+    pregunta: str
+    opciones: List[str]
+    respuesta_correcta: str
+    categoria: Dict[str, Optional[str]]
