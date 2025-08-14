20c13ed6c0c68e8ee4934274f3c2ccfc48f7834b
lazaro20 <salvirialazaro@gmail.com>
2025-08-14 17:27:42 -0300

    Feat(validaciones_generales/lista_campos.py): Agregar los campos de preguntas a la lista de campos.

--- validaciones_generales/lista_campos.py
+++ validaciones_generales/lista_campos.py
@@ -2,5 +2,6 @@ listas_de_campos = {
     "Categoria": ["_id", "nombre_categoria", "descripcion", "tipo", "estado"],
     "Sub_categoria": ["_id", "nombre","categoria_principal", "descripcion", "tipo", "estado"],
     "Micro_categoria": ["_id", "nombre","sub_categoria", "descripcion", "tipo", "estado"],
-    "Nano_categoria": ["_id", "nombre","micro_categoria", "descripcion", "tipo", "estado"]
-}
+    "Nano_categoria": ["_id", "nombre","micro_categoria", "descripcion", "tipo", "estado"],
+    "Preguntas":["_id","pregunta","opciones","respuesta_correcta","categoria", "usuario_carga", "tipo", "estado"]
+}   
