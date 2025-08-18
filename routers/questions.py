1b42188f0ec8fa52972e3ddd3f189a1d2f65cd12
lazaro20 <salvirialazaro@gmail.com>
2025-08-18 11:28:42 -0300

    Feat(routers/questions.py): Agregar la funcion de la peticion put la cual se encarga de asignarles 500 puntos a una pregunta.

--- routers/questions.py
+++ routers/questions.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from db.models.questions import Question
-from db.schemas.nano_categorias import nano_categoria_schema,nano_categorias_schema
+from db.schemas.questions import question_schema, many_question_schema, many_request_schema
 from peticiones_http import (
     peticiones_http_post,
-    peticiones_http_get
+    peticiones_http_get, 
+    peticiones_http_put
 )
 from validaciones import validar_questions
 
@@ -28,3 +29,15 @@ peticiones_http_post.realizar_carga_masiva(
     base_de_datos,
     validar_questions.validacion_carga_question
 )
+
+peticiones_http_get.view_old_data(
+    router, 
+    base_de_datos, 
+    Question, 
+    many_question_schema
+)
+
+peticiones_http_put.agregar_puntos(
+    router, 
+    base_de_datos, 
+    many_question_schema)
