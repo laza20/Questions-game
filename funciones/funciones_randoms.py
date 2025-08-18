59d7dfa53b0bc7883baed6bb315ab106f72e0c5a
lazaro20 <salvirialazaro@gmail.com>
2025-08-18 13:54:34 -0300

    Feat(funciones/funciones_randoms.py): Crear un archivo el cual se encargue de aleatorizar las preguntas.

---
+++ funciones/funciones_randoms.py
@@ -0,0 +1,43 @@
+import random
+from db.client import db_client
+from fastapi import HTTPException, status
+
+#Sirve para cuando se quiere jugar a una categoria sola.
+def aleatorizar_preguntas_de_una_categoria():
+    nivel_elegido = aleatorizar_niveles()
+    return nivel_elegido
+
+
+#Sirve para cuando se quiere jugar en todas las categorias.
+def aleatorizar_preguntas_generales():
+    categoria_elegida = aleatorizar_categorias_generales()
+    nivel_elegido     = aleatorizar_niveles()
+    
+    return nivel_elegido, categoria_elegida
+
+def aleatorizar_pregunta(base_de_datos, schema):
+    nivel_elegido, categoria_elegida = aleatorizar_preguntas_generales()
+    coleccion = getattr(db_client, base_de_datos)
+    documentos = schema(coleccion.find({"nivel":nivel_elegido, "categoria":categoria_elegida}))
+    
+    #Momentaneo, en una actualizacion pronta pienso implementar una funcion recursiva que vuelva a buscar otros filtros.
+    if not documentos:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="No hay documentos de este tipo.")
+    
+    pregunta_elegida = random.choice(documentos)
+    return pregunta_elegida
+
+
+def aleatorizar_niveles():
+    niveles = ['facil', 'medio', 'dificil', 'imposible']
+    probabilidades = [0.40, 0.30, 0.20, 0.10]
+    
+    nivel_elegido = random.choices(niveles, weights=probabilidades, k=1)
+    return nivel_elegido[0]
+
+def aleatorizar_categorias_generales():
+    categorias = ["deportes","Entretenimiento","ciencia", "historia", "cultura general"]
+    
+    categoria_elegida = random.choices(categorias)
+    
+    return categoria_elegida[0]
