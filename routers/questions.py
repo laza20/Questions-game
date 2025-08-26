2ad4a4c700d7205118c2e82d9d5ba7c57fb2007a
lazaro20 <salvirialazaro@gmail.com>
2025-08-26 13:59:43 -0300

    Refactor(routers/questions.py): Eliminar las llamadas a las funciones.

--- routers/questions.py
+++ routers/questions.py
@@ -1,13 +1,7 @@
 from fastapi import APIRouter
 from db.models.questions import Question
 from db.schemas.questions import question_schema, many_question_schema, many_request_schema
-from peticiones_http import (
-    peticiones_http_post,
-    peticiones_http_get, 
-    peticiones_http_put,
-    peticiones_http_delete
-)
-from validaciones import validar_questions
+from service import service_questions
 
 
 
@@ -17,54 +11,3 @@ router = APIRouter(prefix="/Preguntas",
 )
 base_de_datos = "Preguntas"
 
-peticiones_http_post.realizar_carga_unitaria(
-    Question,
-    router,
-    base_de_datos,
-    validar_questions.validacion_carga_question
-)
-
-peticiones_http_post.realizar_carga_masiva(
-    Question,
-    router,
-    base_de_datos,
-    validar_questions.validacion_carga_question
-)
-
-peticiones_http_get.view_old_data(
-    router, 
-    base_de_datos, 
-    Question
-)
-
-peticiones_http_put.agregar_puntos(
-    router, 
-    base_de_datos
-    )
-
-
-peticiones_http_put.modificar_puntos_by_id(
-    router, 
-    base_de_datos
-)
-
-peticiones_http_get.view_question_random(
-    router, 
-    base_de_datos,  
-    Question
-    )
-
-peticiones_http_get.play_question_general_random(
-    router, 
-    base_de_datos
-)
-
-peticiones_http_get.play_question_category(
-    router, 
-    base_de_datos
-)
-
-peticiones_http_delete.delete_old_by_type(
-    router, 
-    base_de_datos
-)
