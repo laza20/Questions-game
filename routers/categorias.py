3d0e214b7d456a294efb4a269eb2020e519cfac7
lazaro20 <salvirialazaro@gmail.com>
2025-08-26 13:59:00 -0300

    Feat(routers/categorias.py): Crear el router para realizar las cargas, ya sea una o muchas cargas.

--- routers/categorias.py
+++ routers/categorias.py
@@ -2,32 +2,18 @@ from fastapi import APIRouter
 from db.models.categorias import Categoria
 from service import service_categorias
 from db.models.categorias import Categoria
-
-
+from fastapi import HTTPException, status, Body
 
 router = APIRouter(prefix="/Categoria",
                    tags=["CATEGORIA"],
                    responses={404:{"Message":"No encontrado"}}
 )
-base_de_datos = "Categoria"
+base_de_datos = "Categorias"
 
-
-
-
-
-
-
-
-
-peticiones_http_get.view_old_data(
-    router, 
-    base_de_datos, 
-    Categoria
-    )
-
-lista_de_propiedades = ["nombre", "descripcion"] 
-peticiones_http_get.view_one_document_for_data_str(
-    router, 
-    base_de_datos, 
-    lista_de_propiedades
-    )
+@router.post("/Realizar/Carga", response_model=list[Categoria], status_code=status.HTTP_201_CREATED)
+async def crear_categorias_endpoint(categorias: list[Categoria] = Body(...)):
+    """
+    Crea una o varias categorías en la base de datos.
+    """
+    nuevas_categorias = service_categorias.insertar_categorias(categorias)
+    return nuevas_categorias
