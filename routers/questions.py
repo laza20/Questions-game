812cec7e781ebd34a099a2ab590268fe43a27ff8
lazaro20 <salvirialazaro@gmail.com>
2025-08-27 13:26:44 -0300

    Feat(routers/questions.py): Crear un router para poder realizar la carga de preguntas ya sea una o muchas.

--- routers/questions.py
+++ routers/questions.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from db.models.questions import Question
-from db.schemas.questions import question_schema, many_question_schema, many_request_schema
+from db.models.questions import Question, QuestionRequest
 from service import service_questions
+from fastapi import status, Body
+from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
 
 
 
@@ -10,4 +12,13 @@ router = APIRouter(prefix="/Preguntas",
                    responses={404:{"Message":"No encontrado"}}
 )
 base_de_datos = "Preguntas"
+
+@router.post("/Realizar/Carga", response_model=list[QuestionRequest], status_code=status.HTTP_201_CREATED)
+async def crear_questions_endpoint(question: list[Question] = Body(...)):
+    """
+    Crea una o varias categorías en la base de datos.
+    """
+    nuevas_preguntas = service_questions.insertar_question(question)
+    return nuevas_preguntas 
+
 
