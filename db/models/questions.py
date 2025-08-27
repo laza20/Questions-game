af5b7d2ea22967acbf562dcb16367254328faaa8
lazaro20 <salvirialazaro@gmail.com>
2025-08-27 13:26:04 -0300

    Feat(db/models/questions.py): Modificar el modelo pydantic de questions para no precisar formular el dict por fuera y que los datos no cargados se impregnen de manera inmediata.

--- db/models/questions.py
+++ db/models/questions.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import List, Optional, Dict
 from pydantic import BaseModel, Field
 
@@ -8,17 +8,18 @@ class Question(BaseModel):
     opciones           : List[str]
     respuesta_correcta : str
     categoria_id       : str
-    puntos_pregunta    : Optional[int]      = None
-    nivel              : Optional[str]      = None
-    consecutiva        : Optional[int]      = None
-    usuario_carga      : Optional[str]      = None
-    fecha_carga        : Optional[datetime] = None
-    tipo               : Optional[str]      = None
-    estado             : Optional[bool]     = None
+    puntos_pregunta    : Optional[int]      = 500
+    nivel              : Optional[str]      = "Medio"
+    consecutiva        : Optional[int]      = 0
+    usuario_carga      : Optional[str]      = "Master"
+    fecha_carga        : Optional[datetime] = Field(default_factory=lambda: datetime.now(timezone.utc))
+    tipo               : Optional[str]      = "Preguntas"
+    estado             : Optional[bool]     = True
 
 class QuestionRequest(BaseModel):
     pregunta          : str
     opciones          : List[str]
     respuesta_correcta: str
     nivel             : str
-    categoria         : str
+    categoria_id      : str
+    usuario_carga     : str
