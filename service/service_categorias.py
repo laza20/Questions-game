3108f80bf243e74e503323d06165d6179366cc17
lazaro20 <salvirialazaro@gmail.com>
2025-08-28 10:00:35 -0300

    Feat and Refactor(service/service_categorias.py): Añadir el servicio para visionar los documentos de la coleccion 'Categorias'.

--- service/service_categorias.py
+++ service/service_categorias.py
@@ -9,6 +9,7 @@ from typing import Optional
 from validaciones_generales import validaciones_simples
 from exceptions import errores_simples
 
+"""Inicio de llamadas al usuario """
 def insertar_categorias(categorias: List[Categoria]) -> List[Dict]:
     """
     Función principal para validar e insertar una o varias categorías.
@@ -23,13 +24,18 @@ def insertar_categorias(categorias: List[Categoria]) -> List[Dict]:
 
     for categoria in categorias:
         # 1. Validación de negocio. Llamamos a las validaciones privadas y unificadas.
-        _verificar_grado_categoria(categoria)
-        _validaciones_categoria(categoria)
+        if categoria.grado == "Primer":
+            _verificar_limite_categoria_principal()
+            
+        oid_padre = None
+        if categoria.grado != "Primer" and categoria.padre_id:
+                    oid_padre = db_helpers.get_categoria_id(categoria.padre_id)
         
         # 2. Verificar duplicados en la carga masiva
-        key = _create_key(categoria)
+        key = _create_key(categoria, oid_padre)
         if key in nombres_cargados:
             errores_simples.error_carga_repetida_un_solo_dato(categoria.nombre, "Categorias")
+        nombres_cargados.add(key)
             
         nombres_cargados.add(key)
         
@@ -53,13 +59,27 @@ def insertar_categorias(categorias: List[Categoria]) -> List[Dict]:
             
     return documentos
 
-# --- Funciones de validación privadas, ahora unificadas aquí ---
 
-def _verificar_grado_categoria(categoria: Categoria):
-    if categoria.grado == "Primer":
-        _verificar_limite_categoria_principal()
+def visionar_todas_las_categorias()-> List[Dict]:
+    documentos = list(db_client.Categorias.find({"tipo":"Categoria"}))
+    if not documentos:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, 
+            detail=f"No se encontraron las categorias necesarias"
+            )
+    
+    documentos_formateados = [_format_document(doc) for doc in documentos]
+    return documentos_formateados
+
+
+""" Fin de llamadas al usuario """
+
+
+""" Funciones privadas de este archivo """
 
 def _verificar_limite_categoria_principal():
+    """Funcion encargada de verificar si una categoria es principal y en 
+    caso de serlo verifica que no haya mas de 5 de este grado."""
     cantidad = db_client.Categorias.count_documents({"grado": "Primer"})
     if cantidad >= 5:
         raise HTTPException(
@@ -74,14 +94,17 @@ def _validaciones_categoria(dato: Categoria, oid_padre: Optional[ObjectId] = Non
         validaciones_simples.validacion_simple_general_negativa("Categorias", oid_padre)
 
 def _create_key(dato: Categoria, oid_padre: Optional[ObjectId] = None):
+    """Funcion encargada de generar una key la cual permite verificar que los documentos
+    ingresados no se repitan."""
     if dato.grado == "Primer":
         return dato.nombre.lower()
     else:
-        return (dato.nombre.lower(), oid_padre)
+        return (dato.nombre.lower(), str(oid_padre))
     
 def _format_document(doc: Dict) -> Dict:
     """Funcion que formatea el id para entregar un str en lugar de un object id."""
     if doc:
         doc["id"] = str(doc.pop("_id"))
-        doc["padre_id"] = str(doc["padre_id"])
+        if "padre_id" in doc and doc["padre_id"] is not None:
+            doc["padre_id"] = str(doc["padre_id"])
     return doc
