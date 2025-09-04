954156be6aab055d5f558944500b2ae0de4aff3e
lazaro20 <salvirialazaro@gmail.com>
2025-09-04 16:18:25 -0300

    Feat(main.py): Agregar el router de usuarios.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from routers import (
     categorias,
-    questions
+    questions,
+    usuarios
 )
 
 
@@ -9,3 +10,4 @@ app = FastAPI()
 
 app.include_router(categorias.router)
 app.include_router(questions.router)
+app.include_router(usuarios.router)
