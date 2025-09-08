ce3d2c5d3da120c9bd6c906b24ad819b61d19c40
lazaro20 <salvirialazaro@gmail.com>
2025-09-08 09:47:06 -0300

    Feat(routers/usuarios.py): Crear un router para el logeo de usuario.

--- routers/usuarios.py
+++ routers/usuarios.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter,HTTPException, status
+from fastapi import APIRouter,HTTPException, status, Depends
 from db.models.usuarios import Usuario, UsarioLogeado, UsuarioCreado
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from service import service_usuarios
 
 router = APIRouter(prefix="/Usuarios",
@@ -16,4 +17,12 @@ async def create_user(user:Usuario):
     usuario = service_usuarios.insert_users(user)
     return usuario
 
+
+@router.post("/Login", status_code=status.HTTP_202_ACCEPTED)
+async def logearse(usuario:OAuth2PasswordRequestForm = Depends()):
+    """
+    End point que sirve para loguearse el usuario.
+    """
+    logeado = service_usuarios.login_user(usuario)
+    return logeado
 
