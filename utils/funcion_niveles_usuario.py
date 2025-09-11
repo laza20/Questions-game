eb7b83b9d105f3ade5044334b54103a3452a5185
lazaro20 <salvirialazaro@gmail.com>
2025-09-11 16:23:59 -0300

    Feat(utils/funcion_niveles_usuario.py): Crear un modulo el cual sirva para actualizar el nivel del usuario.

---
+++ utils/funcion_niveles_usuario.py
@@ -0,0 +1,151 @@
+def niveles_usuario(experiencia):
+    if experiencia <= 110:
+        nivel = 1
+        
+    elif experiencia <= 220:
+        nivel = 2
+        
+    elif experiencia <= 330:
+        nivel = 3
+        
+    elif experiencia <= 440:
+        nivel = 4
+        
+    elif experiencia <= 550:
+        nivel = 5
+        
+    elif experiencia <= 660:
+        nivel = 6
+        
+    elif experiencia <= 770:
+        nivel = 7
+        
+    elif experiencia <= 880:
+        nivel = 8
+        
+    elif experiencia <= 990:
+        nivel = 9
+        
+    elif experiencia <= 1100:
+        nivel = 10
+        
+    elif experiencia <= 1220:
+        nivel = 11
+        
+    elif experiencia <= 1340:
+        nivel = 12
+        
+    elif experiencia <= 1460:
+        nivel = 13
+        
+    elif experiencia <= 1580:
+        nivel = 14
+        
+    elif experiencia <= 1700:
+        nivel = 15
+        
+    elif experiencia <= 1820:
+        nivel = 16
+    elif experiencia <= 1940:
+        nivel = 17
+        
+    elif experiencia <= 2060:
+        nivel = 18
+        
+    elif experiencia <= 2180:
+        nivel = 19
+        
+    elif experiencia <= 2300:
+        nivel = 20
+        
+    elif experiencia <= 2430:
+        nivel = 21
+        
+    elif experiencia <= 2560:
+        nivel = 22
+        
+    elif experiencia <= 2690:
+        nivel = 23
+        
+    elif experiencia <= 2820:
+        nivel = 24
+        
+    elif experiencia <= 2950:
+        nivel = 25
+        
+    elif experiencia <= 3080:
+        nivel = 26
+        
+    elif experiencia <= 3210:
+        nivel = 27
+        
+    elif experiencia <= 3330:
+        nivel = 28
+        
+    elif experiencia <= 3470:
+        nivel = 29
+        
+    elif experiencia <= 3600:
+        nivel = 30
+        
+    elif experiencia <= 3740:
+        nivel = 31
+        
+    elif experiencia <= 3880:
+        nivel = 32
+        
+    elif experiencia <= 4020:
+        nivel = 33
+        
+    elif experiencia <= 4160:
+        nivel = 34
+        
+    elif experiencia <= 4300:
+        nivel = 35
+        
+    elif experiencia <= 4440:
+        nivel = 36
+        
+    elif experiencia <= 4580:
+        nivel = 37
+        
+    elif experiencia <= 4720:
+        nivel = 38
+        
+    elif experiencia <= 4860:
+        nivel = 39
+        
+    elif experiencia <= 5000:
+        nivel = 40
+        
+    elif experiencia <= 5150:
+        nivel = 41
+        
+    elif experiencia <= 5300:
+        nivel = 42
+        
+    elif experiencia <= 5450:
+        nivel = 43
+        
+    elif experiencia <= 5600:
+        nivel = 44
+        
+    elif experiencia <= 5750:
+        nivel = 45
+        
+    elif experiencia <= 5900:
+        nivel = 46
+        
+    elif experiencia <= 6150:
+        nivel = 47
+        
+    elif experiencia <= 6300:
+        nivel = 48
+        
+    elif experiencia <= 6450:
+        nivel = 49
+        
+    elif experiencia <= 6600:
+        nivel = 50
+        
+    return nivel
