48340bc6b884ba5738e9f36c9dd0c0c7b854720b
lazaro20 <salvirialazaro@gmail.com>
2025-09-15 15:11:33 -0300

    Feat(service/service_preguntas_respondidas.py): Diseñar un servicio el cual se encargue de realizar la extraccion de las preguntas y la confeccion del diccionario con las estadisticas.

--- service/service_preguntas_respondidas.py
+++ service/service_preguntas_respondidas.py
@@ -17,7 +17,10 @@ from utils import (
 from exceptions import errores_simples
 import random
 
-def view_categories_principals(current_user):
+def view_categories_principals(current_user) -> list[dict]:
+    """
+    Funcion encargada de retornar las estadisticas de las categorias principales
+    """
     preguntas = list(db_client.Preguntas_respondidas.find({"id_usuario": current_user["_id"]}))
     if not preguntas:
         _sin_preguntas()
@@ -64,19 +67,92 @@ def view_categories_principals(current_user):
             porcentaje_final = round(porcentajes*100,2)
         else:
             porcentaje_final = 0
-        dict_porcentajes={"categoria":nombre_categoria,
-                        "cantidad_de_preguntas":total,
-                        "preguntas_acertadas":correcta,
-                        "preguntas_erradas": incorrecta,
-                        "porcentajes": porcentaje_final}
+        
+        dict_porcentajes = _formar_dict_porcentajes(nombre_categoria, total, correcta, incorrecta, porcentaje_final)
         
         lista_porcentajes.append(dict_porcentajes)
         
     return lista_porcentajes                
                 
+def ver_porcentajes_categoria_particular(current_user, categoria):
+    categoria_necesaria = db_client.Categorias.find_one({"nombre":{"$regex": f"^{categoria}$", "$options": "i"}})
+    if not categoria_necesaria:
+        _sin_categorias()
         
+    id_categoria = categoria_necesaria["_id"]
+    nombre_categoria = categoria_necesaria["nombre"]
+    preguntas = list(db_client.Preguntas_respondidas.find({"id_usuario": current_user["_id"]}))
+    if not preguntas:
+        _sin_preguntas()
+
+    categorias_descendentes = db_helpers.identificar_categorias_descentes_con_graphlookup(id_categoria)
+    ids_descendentes = {doc for doc in categorias_descendentes}
+    lista_preguntas = []
+    for pregunta in preguntas:
+        id_pregunta = pregunta["id_pregunta"]
+        pregunta_guardada = db_client.Preguntas.find_one({"_id": id_pregunta})
+        
+        if not pregunta_guardada:
+            continue 
+            
+        name_or_id_categoria = pregunta_guardada.get("categoria_id")
+        
+        id_categoria_asociada = db_helpers.get_categoria_id(name_or_id_categoria)
+        oid_categoria_asociada = funciones_logicas.validate_object_id(id_categoria_asociada)
+        
+        if oid_categoria_asociada not in ids_descendentes:
+            continue
+        
+
+        pregunta_por_categoria = {
+            "pregunta": pregunta,
+            "nombre": nombre_categoria
+        }
+        print(pregunta_por_categoria)
+        
+        lista_preguntas.append(pregunta_por_categoria)
+    
+    dict_porcentajes  = {}
+    correcta = 0
+    incorrecta = 0
+    total = 0
+    porcentaje_final=0
+    for pregunta  in lista_preguntas:
+        
+        if pregunta["pregunta"]["respuesta"] == "CORRECTA":
+            correcta += 1
+        else:
+            incorrecta += 1    
+        
+        total = correcta + incorrecta
+        if total == 0:
+            porcentaje_final = 0
+        else:
+            porcentajes = correcta / total
+            porcentaje_final = round(porcentajes*100,2)
+        
+    dict_porcentajes = _formar_dict_porcentajes(nombre_categoria, total, correcta, incorrecta, porcentaje_final)
+        
+        
+    return dict_porcentajes  
+        
+        
+def _formar_dict_porcentajes(nombre_categoria, total, correcta, incorrecta, porcentaje_final):
+    dict_porcentajes={"categoria":nombre_categoria,
+                    "cantidad_de_preguntas":total,
+                    "preguntas_acertadas":correcta,
+                    "preguntas_erradas": incorrecta,
+                    "porcentajes": porcentaje_final}
+    
+    return dict_porcentajes
     
     
+def _sin_categorias():
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT, 
+        detail=f"No se encontraron las categorias necesarias"
+        )    
+
 def _sin_preguntas():
     raise HTTPException(
         status_code=status.HTTP_409_CONFLICT, 
