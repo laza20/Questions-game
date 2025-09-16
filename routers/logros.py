d67f09b7f58e9716a92448411d541c2ca935d922
lazaro20 <salvirialazaro@gmail.com>
2025-09-16 15:36:37 -0300

    Feat(routers/logros.py): Crear el router de logros ademas de el router para realizar cargas.

---
+++ routers/logros.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from db.models.logros import LogrosGenerales, LogrosUsuario
+from service import service_logros
+from fastapi import HTTPException, status, Body
+from fastapi import APIRouter, Depends
+from dependencias import get_current_user, is_primer_rango, is_segundo_rango, is_tercer_rango
+
+router = APIRouter(prefix="/Logros",
+                   tags=["LOGROS"],
+                   responses={404:{"Message":"No encontrado"}}
+)
+base_de_datos = "Categorias"
+
+@router.post("/Realizar/Carga", response_model=list[LogrosGenerales], status_code=status.HTTP_201_CREATED)
+async def crear_categorias_endpoint(logros: list[LogrosGenerales] = Body(...), current_user: dict = Depends(is_tercer_rango)):
+    """
+    End point encargado de crear uno o varios logros en la base de datos.
+    """
+    nuevos_logros = service_logros.insertar_logros(logros, current_user)
+    return nuevos_logros
