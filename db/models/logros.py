731d5634349299768a2ead4b75403b5d4e2d6879
lazaro20 <salvirialazaro@gmail.com>
2025-09-16 15:36:01 -0300

    Feat(db/models/logros.py): Modificar el modelo de logros para que los datos necesarios deban ser enviados por el usuario mientra que otros se puedan cargar internamente.

--- db/models/logros.py
+++ db/models/logros.py
@@ -3,11 +3,11 @@ from typing import List, Optional, Dict, Any
 from datetime import datetime, timezone
 
 class LogrosGenerales(BaseModel):
-    id: Optional[str] = Field(alias="_id", default=None)
+    id: Optional[str] = None
     nombre: str
     descripcion: str
     puntos: int
-    creador_id: str  # Referencia al usuario que lo creó
+    creador_id: Optional[str] = None  # Referencia al usuario que lo creó
     condicion: Dict[str, Any] # Ej: {"tipo": "duelos_ganados", "valor": 100}
     
 class LogrosUsuario(BaseModel):
