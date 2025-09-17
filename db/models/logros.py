bbbc26ac8d1dcb36c49a5966f5ff722d00c8028c
lazaro20 <salvirialazaro@gmail.com>
2025-09-17 11:45:58 -0300

    Feat(db/models/logros.py): Crear un modelo el cual permita traer el nombre y la descripcion de los logros.

--- db/models/logros.py
+++ db/models/logros.py
@@ -10,7 +10,12 @@ class LogrosGenerales(BaseModel):
     creador_id: Optional[str] = None  # Referencia al usuario que lo creó
     condicion: Dict[str, Any] # Ej: {"tipo": "duelos_ganados", "valor": 100}
     
+class LogrosNames(BaseModel):
+    nombre:str
+    descripcion:str
+    
 class LogrosUsuario(BaseModel):
     id_logro   : str
+    nombre     : str
     id_usuario : str
     fecha_obtenido: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
