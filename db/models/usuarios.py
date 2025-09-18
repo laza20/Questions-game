ca311b682151ab8d55c17841af8776568fd7c775
lazaro20 <salvirialazaro@gmail.com>
2025-09-18 19:57:48 -0300

    Fix(db/models/usuarios.py): Agregar una propiedad para que al crear el usuario el progreso no sea necesario.

--- db/models/usuarios.py
+++ db/models/usuarios.py
@@ -42,7 +42,7 @@ class Usuario(BaseModel):
     descripcion      : Optional[str] = None
     stats            : Stats = Field(default_factory=Stats)
     logros           : Optional[List[str]] = Field(default_factory=list)
-    progreso         : Optional[Progreso]
+    progreso         : Optional[Progreso] = Field(default_factory=list)
     favoritos        : Optional[List[Favorito]] = Field(default_factory=list)
     preguntas        : Optional[List[PreguntasCreadas]] = Field(default_factory=list)
     control          : Control = Field(default_factory=Control)
