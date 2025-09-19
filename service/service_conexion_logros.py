49655507e14fbc3cd3891fb335f27410d6df0909
lazaro20 <salvirialazaro@gmail.com>
2025-09-19 13:35:54 -0300

    Feat(service/service_conexion_logros.py): Modificar la estructura del orquestador.

--- service/service_conexion_logros.py
+++ service/service_conexion_logros.py
@@ -12,36 +12,56 @@ from utils import db_helpers
 from exceptions import errores_simples
 
 
-def orquestador_logros(current_user):
-    preguntas_respondidas = list(db_client.Preguntas_respondidas.find({"id_usuario":current_user["_id"]}))
-    verificador_preguntas_correctas(preguntas_respondidas, current_user)
+def orquestador_logros(current_user, evento, datos_evento):
+    """
+    Funcion orquestadora, recibe, el usuario que esta jugando, 
+    el evento (pregunta respondida, duelo ganado) 
+    y datos del evento (categoria, respuesta)
+    """
+    
+    if evento == "pregunta_respondida":
+        es_correcta = datos_evento.get("es_correcta", False)
+        categoria = datos_evento.get("categoria")
+        dificultad = datos_evento.get("dificultad")
 
-    
-    
-def verificador_preguntas_correctas(preguntas_respondidas, current_user):
-    logros = list(db_client.Logros.find({"tipo":"Logro", "condicion.tipo":"preguntas_correctas"}))
-    contador = 0
-    for pregunta in preguntas_respondidas:
-        if pregunta["respuesta"] == "CORRECTA":
-            contador += 1
-    
-    lista_logros = []
+        if es_correcta:
+            db_client.Usuarios.update_one(
+                {"_id": current_user["_id"]},
+                {"$inc": {"progreso.preguntas_correctas": 1}}
+            )
+            
+            if categoria:
+                campo_categoria = f"progreso.preguntas_{categoria}_correctas"
+                db_client.Usuarios.update_one(
+                    {"_id": current_user["_id"]},
+                    {"$inc": {campo_categoria: 1}}
+                )
+            if dificultad:
+                campo_dificultad = f"progreso.preguntas_{dificultad}_correctas"
+                db_client.Usuarios.update_one(
+                    {"_id": current_user["_id"]},
+                    {"$inc": {campo_dificultad: 1}}
+                )
+        
+        usuario_actualizado = db_client.Usuarios.find_one({"_id": current_user["_id"]})
+        
+        # Trigger all relevant verifiers
+        verificador_preguntas_correctas(usuario_actualizado)
+
+def verificador_preguntas_correctas(current_user):
+    logros = list(db_client.Logros.find({"condicion.tipo": "preguntas_correctas"}))
+    contador = current_user["progreso"]["preguntas_correctas"]
+
     for logro in logros:
-        if logro["condicion"]["valor"] <= contador:
-            lista_logros.append(logro)
-    
-    for logro in lista_logros:
-        if logro["_id"] in current_user["logros"]:
-            continue
-        else:
-            current_user["logros"].append(logro["_id"])
-        
-        
+        if str(logro["_id"]) not in [str(l) for l in current_user.get("logros", [])]:
+            if logro["condicion"]["valor"] <= contador:
+                current_user["logros"].append(logro["_id"])
+
     db_client.Usuarios.update_one(
         {"_id": current_user["_id"]},
         {"$set": {"logros": current_user["logros"]}}
     )
-    
+
     return current_user
                 
  
