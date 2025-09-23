1a4434d524ebe30284c72e739c839ecbb6c139c7
lazaro20 <salvirialazaro@gmail.com>
2025-09-23 15:08:34 -0300

    Feat(service/service_conexion_logros.py): Crear una sola funcion por medio de la cual todos los logros de preguntas (categoria, nivel, preguntas correctas) seran cargados.

--- service/service_conexion_logros.py
+++ service/service_conexion_logros.py
@@ -1,17 +1,12 @@
 from db.client import db_client
 from utils import db_helpers
+from bson.objectid import ObjectId
 
 def orquestador_logros(current_user, evento, datos_evento):
     usuario_actualizado = db_client.Usuarios.find_one({"_id":current_user["_id"]})
     if evento == "pregunta_respondida":
         if datos_evento.get("respuesta_acertada") == "CORRECTA":
-            
-            campo_categoria        = _obtener_campo_categoria(datos_evento)
-            campo_dificultad_final = _obtener_campo_nivel(datos_evento)
-            
-            verificador_preguntas_correctas(usuario_actualizado)
-            logros_niveles(usuario_actualizado, campo_dificultad_final)
-            logros_categorias(usuario_actualizado, campo_categoria)
+            logros_preguntas(usuario_actualizado)
             logros_puntos(usuario_actualizado)
             logros_nivel_usuario(usuario_actualizado)
 
@@ -40,43 +35,29 @@ def logros_puntos(current_user):
     otorgar_logro(current_user, logros_de_puntos, contador)
     return current_user
 
-def logros_niveles(current_user, campo_dificultad):
+def logros_preguntas(current_user):
     """
-    Funcion encargada de obtener:
-    las preguntas correctas del usuario de un nivel determinado.
-    los logros de condicion.tipo: ejemplo: preguntas_facil_correctas.
+    Funcion encargada de cargar todos los logros de preguntas.
     """
-    logros_nivel = list(db_client.Logros.find({"condicion.tipo": campo_dificultad}))
-    contador = current_user["progreso"].get(campo_dificultad, 0)
-    otorgar_logro(current_user, logros_nivel, contador)
+    logros = list(db_client.Logros.find({"condicion.tipo": {"$regex": "^preguntas_.*"}}))
+    user_logros = set(str(l) for l in current_user.get("logros", []))
+    logros_nuevos = []
+    for logro in logros:
+        condicion = logro["condicion"]["tipo"]
+        if logro["condicion"]["tipo"] != "maximo" and condicion != "preguntas_creadas":
+            if logro["_id"] not in user_logros and logro["condicion"]["valor"] <= current_user["progreso"][condicion]:
+                logros_nuevos.append(logro["_id"])
+                current_user["logros"].append(ObjectId(logro["_id"]))
+
+    if logros_nuevos:
+        db_client.Usuarios.update_one(
+            {"_id": current_user["_id"]},
+            {"$addToSet": {"logros": {"$each": logros_nuevos}}}
+        )
+                    
     return current_user
 
-
-
-def logros_categorias(current_user, campo_categoria):
-    """
-    Funcion encargada de obtener:
-    las preguntas correctas del usuario en una categoria.
-    los logros de condicion.tipo: ejemplo: preguntas_historia_correctas.
-    """
-    logros_por_categoria = list(db_client.Logros.find({"condicion.tipo": campo_categoria.replace("progreso.", "")}))
-    campo_categoria_key = campo_categoria.replace("progreso.", "")
-    contador = current_user["progreso"].get(campo_categoria_key, 0)
-    otorgar_logro(current_user, logros_por_categoria, contador)
-    return current_user
-
-def verificador_preguntas_correctas(current_user):
-    """
-    Funcion encargada de obtener:
-    las preguntas correctas del usuario.
-    los logros de condicion.tipo: preguntas_correctas.
-    """
-    logros = list(db_client.Logros.find({"condicion.tipo": "preguntas_correctas"}))
-    contador = current_user["progreso"]["preguntas_correctas"]
-    otorgar_logro(current_user, logros, contador)
-    return current_user
-
-
+    
 
 def otorgar_logro(current_user, logros, contador):
     """
@@ -96,30 +77,3 @@ def otorgar_logro(current_user, logros, contador):
     return current_user
 
  
-def _obtener_campo_categoria(datos_evento):
-    """
-    Funcion encargada de recibir los datos, y devolver el campo de la categoria que se va a utilizar.
-    ejemplo de return: progreso.preguntas_deportes_correctas
-    """
-    if datos_evento["categoria_id"]:
-        id_categoria = db_helpers.get_categoria_id(datos_evento["categoria_id"])
-        categoria_principal = db_helpers.identificar_categoria_con_graphlookup(id_categoria)
-        nombre_categoria_principal = categoria_principal["nombre"]
-    
-    if categoria_principal:
-        campo_categoria = f"progreso.preguntas_{nombre_categoria_principal}_correctas"
-        
-    return campo_categoria
-    
-def _obtener_campo_nivel(datos_evento):
-    """
-    Funcion encargada de recibir los datos, y devolver el campo del nivel que se va a utilizar.
-    ejemplo de return: progreso.preguntas_muy_facil_correctas
-    ejemplo de return: progreso.preguntas_facil_correctas
-    """
-    dificultad = datos_evento.get("nivel")
-    if dificultad:
-        campo_dificultad_normalizado = db_helpers.verificador_nivel(dificultad)
-        campo_dificultad_final = f"preguntas_{campo_dificultad_normalizado}_correctas"
-        
-    return campo_dificultad_final
