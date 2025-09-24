58504e7d67ee73ce5c7631990f658750d480c518
lazaro20 <salvirialazaro@gmail.com>
2025-09-24 12:13:55 -0300

    Feat(routers/usuarios.py): Crear un end point para eliminar un logro de un usuario.

--- routers/usuarios.py
+++ routers/usuarios.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,HTTPException, status, Depends
 from db.models.usuarios import Usuario, UsarioLogeado, UsuarioCreado
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from service import service_usuarios
+from dependencias import get_current_user, is_primer_rango, is_segundo_rango, is_tercer_rango
 
 router = APIRouter(prefix="/Usuarios",
                    tags=["Usuarios"],
@@ -26,3 +27,10 @@ async def logearse(usuario:OAuth2PasswordRequestForm = Depends()):
     logeado = service_usuarios.login_user(usuario)
     return logeado
 
+@router.patch("/Eliminar/Logro/{id}", response_model=Usuario, status_code=status.HTTP_202_ACCEPTED)
+async def delete_achievement_by_id_for_user(id:str, current_user: dict = Depends(is_primer_rango)):
+    """
+    End point encargado de eliminar un logro por medio de su id
+    """
+    usuario = service_usuarios.eliminar_logro(id, current_user)
+    return usuario
