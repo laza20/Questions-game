55edb95f10463bf042cc969e50f44fb77b5d4df7
lazaro20 <salvirialazaro@gmail.com>
2025-09-24 12:59:32 -0300

    Feat(db/models/usuario_player.py): Crear un modelo para mostrar los logros de un usuario.

--- db/models/usuario_player.py
+++ db/models/usuario_player.py
@@ -1,8 +1,15 @@
 from pydantic import BaseModel, Field, EmailStr
 from db.models.usuarios import UsuarioPlayer
 from db.models.questions import Respuesta
+from db.models.logros import LogrosNames
+
 
 class RespuestaUsuario (BaseModel):
     usuario   : UsuarioPlayer
     respuesta : Respuesta
+    
+    
+class LogrosMostrar(BaseModel):
+    usuario : UsuarioPlayer
+    logros  : list[LogrosNames]
 
