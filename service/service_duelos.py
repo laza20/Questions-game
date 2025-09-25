4ca505f429d0a6d8cc2ba40070d0e7b7a6ab491b
lazaro20 <salvirialazaro@gmail.com>
2025-09-25 12:22:40 -0300

    Feat(service/service_duelos.py): Crear un servicio para iniciar el duelo.

---
+++ service/service_duelos.py
@@ -0,0 +1,31 @@
+# In services/categorias_service.py
+from typing import Dict, List
+from fastapi import HTTPException, status
+from db.models.categorias import Categoria
+from db.client import db_client
+from bson.objectid import ObjectId
+from utils import db_helpers, funciones_logicas
+from typing import Optional
+from validaciones_generales import validaciones_simples
+from exceptions import errores_simples
+
+
+
+def iniciar_duelo(current_user:dict) -> Dict:
+    duelo = {"usuario_uno_id": current_user["_id"],
+             }
+    
+    id = db_client.Duelos.insert_one(duelo).inserted_id
+    nuevo_duelo = db_client.Duelos.find_one({"_id": id})
+    print(nuevo_duelo)
+    duelo_transformado = _transformar_id(nuevo_duelo)
+    return duelo_transformado
+    
+    
+    
+def _transformar_id(doc: Dict) -> Dict:
+    """Funcion encargada de formatear el id para entregar un str en lugar de un object id."""
+    if doc:
+        doc["id"] = str(doc.pop("_id"))
+        doc["usuario_uno_id"] = str(doc["usuario_uno_id"])
+    return doc
